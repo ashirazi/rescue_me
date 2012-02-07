a64a01d5c035dfcda448e123836838ba2ceb5cfd
Rémy Coutable <remy@jilion.com>
2012-02-07 11:27:51 +0100

    Fixes a "NoMethodError: private method `warn' called for nil:NilClass" error
    Use Rails.logger if available, otherwise use Std Lib Logger.

--- lib/rescue_me.rb
+++ lib/rescue_me.rb
@@ -10,21 +10,48 @@ module Kernel
   #       strongly encouraged to provide arguments that capture temporary
   #       exceptional conditions that are likely to work upon a retry.
   def rescue_and_retry(max_attempts=7, *temporary_exceptions)
-    retry_interval = 2    # A good initial start value. Tweak as needed.
+    retry_interval = 2 # A good initial start value. Tweak as needed.
     temporary_exceptions << Exception if temporary_exceptions.empty?
-    begin 
+    begin
       yield
     rescue *temporary_exceptions => e
       attempt = (attempt || 0) + 1
-      message = "rescue and retry (attempt #{attempt}/#{max_attempts}): " +
-          "#{caller.first},  <#{e.class}: #{e.message}>"
-      # TODO AS: Is there a better way to access and use a logger?
-      (defined? logger) ? logger.warn(message) : puts(message)
+      RescueMe::Logger.warn(attempt, max_attempts, caller, e)
+
       raise(e) if attempt >= max_attempts
+
       # Retry immediately before exponential waits (1, 2, 4, 16, ... sec)
       sleep retry_interval**(attempt - 2) if attempt >= 2
       retry
     end
+  end
+
+end
+
+module RescueMe
+  require "logger"
+
+  class << self
+    def logger
+      @logger ||= defined?(Rails) && Rails.respond_to?(:logger) ? Rails.logger : ::Logger.new($stdout)
+    end
+  end
+
+  module Logger
+
+    class << self
+      def warn(attempt, max_attempts, kaller, exception)
+        if RescueMe.logger && RescueMe.logger.respond_to?(:warn)
+          RescueMe.logger.warn(rescued_message(attempt, max_attempts, kaller, exception))
+        end
+      end
+
+      def rescued_message(attempt, max_attempts, kaller, exception)
+        "rescue and retry (attempt #{attempt}/#{max_attempts}): " +
+        "#{kaller.first}, <#{exception.class}: #{exception.message}>"
+      end
+    end
+
   end
 
 end
